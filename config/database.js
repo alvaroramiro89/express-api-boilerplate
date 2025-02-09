483c7bb04dc64842d864553622f1b5f6d364d860
Alvaro De la Iglesia Niveyro <alvarodelaiglesia@hotmail.com>
2025-02-09 23:26:35 +0100

    Initial commit - RealWorld API working

---
+++ config/database.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    dialect: "postgres",
+    url: process.env.DATABASE_URL
+  },
+  test: {
+    dialect: "postgres",
+    url: process.env.DATABASE_URL
+  },
+  production: {
+    dialect: "postgres",
+    url: process.env.DATABASE_URL
+  }
+};
+
